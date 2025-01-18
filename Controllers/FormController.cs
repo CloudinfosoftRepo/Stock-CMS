c4c2ac65b616b38cef3d6695bcaf69b51d12b2b7
Yash Barot <byash140@gmail.com>
2025-01-18 13:10:10 +0530

    Form Stock

---
+++ Controllers/FormController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Stock_CMS.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        } 
+        public IActionResult ChangeOfAddress()
+        {
+            return View();
+        }  
+        public IActionResult AFFIDAVITChangeOfAddress()
+        {
+            return View();
+        }        
+		public IActionResult AFFIDAVIT003COA()
+		{
+			return View();
+		}   
+        public IActionResult AffidavitApplicantNameCorrection()
+        {
+            return View();
+        }    
+        public IActionResult AFFIDAVITChangeofNameNewLinkinTime()
+        {
+            return View();
+        }        
+        public IActionResult AFFIDAVITDeceasedHolder()
+        {
+            return View();
+        }
+		public IActionResult AffidavitforCOSNewLinkinTime()
+		{
+			return View();
+		}
+        public IActionResult AffidavitforNameVariation()
+		{
+			return View();
+		}
+        public IActionResult AFFIDAVITPedigreeLegalHierCerti()
+		{
+			return View();
+		}
+        public IActionResult AgreementasRepresentativeof()
+		{
+			return View();
+		}
+        public IActionResult ChangeofAdd()
+		{
+			return View();
+		}  
+        public IActionResult ChangeofAddressCorrespondence()
+		{
+			return View();
+		}  
+        public IActionResult DocumentsList()
+		{
+			return View();
+		}     
+        public IActionResult DupAffA()
+		{
+			return View();
+		} 
+        public IActionResult DupIndemnityB()
+		{
+			return View();
+		}  
+		public IActionResult FormISR1()
+		{
+			return View();
+		}
+		public IActionResult FormISR2()
+		{
+			return View();
+		}		
+		public IActionResult FormISR3()
+		{
+			return View();
+		}
+        public IActionResult FormISR4()
+        {
+            return View();
+        } 
+		public IActionResult FormISR13()
+        {
+            return View();
+        }
+        public IActionResult IEPFinfo()
+        {
+            return View();
+        }
+        public IActionResult NameChangeAfterMrg()
+        {
+            return View();
+        }
+    }
+}
